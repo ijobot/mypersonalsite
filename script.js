b66d3559f258d4ac21f2ab87f6e742750055e90d
Joe Loftus <72152720+ijobot@users.noreply.github.com>
2021-05-25 17:48:26 +0100

    Add files via upload
    Another massive change.   Redesigned landing page and created elegant theme as standard option.  Further developed and updated dark and light themes.  Will add confetti and neon soon.

--- script.js
+++ script.js
@@ -3,18 +3,26 @@ const themeMenu = document.querySelector('.themes-menu')
 const cssSheet = document.getElementById('css')
 const menuLink = document.querySelectorAll('.menu-link')
 
-
+const themeOptionElegant = document.querySelector('#elegant')
 const themeOptionDark = document.querySelector('#dark')
 const themeOptionLight = document.querySelector('#light')
 const themeOptionNeon = document.querySelector('#neon')
 const themeOptionConfetti = document.querySelector('#confetti')
 
-const userTheme = localStorage.getItem('user_theme') || './darktheme.css'
+const userTheme = localStorage.getItem('user_theme') || './eleganttheme.css'
 
 cssSheet.href = userTheme
 
 themeBtn.addEventListener('click', () => {
     themeMenu.classList.toggle('open')
+})
+
+themeBtn.addEventListener('mouseup', () => {
+    themeBtn.blur()
+})
+
+themeMenu.addEventListener('click', () => {
+    themeMenu.classList.remove('open')
 })
 
 themeOptionDark.addEventListener('click', () => {
@@ -39,6 +47,12 @@ themeOptionConfetti.addEventListener('click', () => {
     cssSheet.href = "./confettitheme.css"
     themeMenu.classList.remove('open')
     localStorage.setItem('user_theme', './confettitheme.css')
+})
+
+themeOptionElegant.addEventListener('click', () => {
+    cssSheet.href = "./eleganttheme.css"
+    themeMenu.classList.remove('open')
+    localStorage.setItem('user_theme', './eleganttheme.css')
 })
 
 // alert('This page is still under construction and does not represent a finished product.')
